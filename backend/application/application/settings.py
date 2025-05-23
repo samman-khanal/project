7511f035b427fba32017c19941bdc73aff28d96e
samman-khanal <samman.samman345@gmail.com>
2025-05-23 18:14:53 +0545

    My second commit. Added dependencies.

--- backend/application/application/settings.py
+++ backend/application/application/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Third-party apps
+    'authentication',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
